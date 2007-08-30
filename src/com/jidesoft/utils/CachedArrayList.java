0cd40b1cb8c4a15d84d19269eb1d087c952ec4c5
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2007-08-30 19:44:36 +0000

    make CachedArrayList perform better
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@220 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/utils/CachedArrayList.java
+++ src/com/jidesoft/utils/CachedArrayList.java
@@ -8,6 +8,7 @@ package com.jidesoft.utils;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
 /**
  * This is a fast access ArrayList that sacrifices memory for speed. It will
@@ -30,9 +31,7 @@ public class CachedArrayList<E> extends ArrayList<E> {
 
     @Override
     public int indexOf(Object elem) {
-        if (_indexCache == null) {
-            _indexCache = new HashMap();
-        }
+        initializeCache();
         Integer o = _indexCache.get(elem);
         if (o != null) {
             return o;
@@ -44,47 +43,93 @@ public class CachedArrayList<E> extends ArrayList<E> {
         }
     }
 
+    /**
+     * Adjusts the cache so that all values that are greater than index will increase by the value specified by the increase parameter.
+     *
+     * @param index    the index. All values above this index will be changed.
+     * @param increase a positive number to increase or a negative number to decrease.
+     */
+    protected void adjustCache(int index, int increase) {
+        if (_indexCache != null) {
+            Set<Object> keys = _indexCache.keySet();
+            for (Object key : keys) {
+                int value = _indexCache.get(key);
+                if (value > index) {
+                    _indexCache.put(key, value + increase);
+                }
+            }
+        }
+    }
+
     @Override
     public boolean add(E o) {
-        invalidateCache();
-        return super.add(o);
+        boolean added = super.add(o);
+        if (added) {
+            initializeCache();
+            _indexCache.put(o, size() - 1);
+        }
+        return added;
     }
 
     @Override
     public void add(int index, E element) {
-        invalidateCache();
         super.add(index, element);
+        adjustCache(index, 1);
+        initializeCache();
+        _indexCache.put(element, index);
+    }
+
+    private void initializeCache() {
+        if (_indexCache == null) {
+            _indexCache = new HashMap();
+        }
     }
 
     @Override
     public E remove(int index) {
-        invalidateCache();
-        return super.remove(index);
+        E element = super.remove(index);
+        if (element != null) {
+            adjustCache(index, -1);
+        }
+        return element;
     }
 
     @Override
     public boolean remove(Object o) {
-        invalidateCache();
-        return super.remove(o);
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        invalidateCache();
-        return super.addAll(c);
+        int oldIndex = indexOf(o);
+        boolean removed = super.remove(o);
+        if (removed) {
+            adjustCache(oldIndex, -1);
+        }
+        return removed;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
-        invalidateCache();
-        return super.addAll(index, c);
+        boolean added = super.addAll(index, c);
+        initializeCache();
+        adjustCache(index, c.size());
+        for (E e : c) {
+            _indexCache.put(e, index++);
+        }
+        return added;
     }
 
     @Override
     public E set(int index, E element) {
-        return super.set(index, element);
+        initializeCache();
+        Object old = _indexCache.get(get(index));
+        E e = super.set(index, element);
+        if (old != null) {
+            _indexCache.remove(old);
+        }
+        _indexCache.put(e, index);
+        return e;
     }
 
+    /**
+     * Invalidated the whole cache.
+     */
     public void invalidateCache() {
         _indexCache = null;
     }
