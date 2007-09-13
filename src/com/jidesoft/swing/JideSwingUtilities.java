b2c0b69fc25d36640af8760132384442353c36f6
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2007-09-13 20:10:13 +0000

    add getFasterScaleInstance and use it in ImagePreviewList
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@241 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/swing/JideSwingUtilities.java
+++ src/com/jidesoft/swing/JideSwingUtilities.java
@@ -486,6 +486,102 @@ public class JideSwingUtilities implements SwingConstants {
         }
     }
 
+    /**
+     * Convenience method that returns a scaled instance of the
+     * provided BufferedImage.
+     *
+     * @param img                 the original image to be scaled
+     * @param targetWidth         the desired width of the scaled instance,
+     *                            in pixels
+     * @param targetHeight        the desired height of the scaled instance,
+     *                            in pixels
+     * @param hint                one of the rendering hints that corresponds to
+     *                            RenderingHints.KEY_INTERPOLATION (e.g.
+     *                            RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR,
+     *                            RenderingHints.VALUE_INTERPOLATION_BILINEAR,
+     *                            RenderingHints.VALUE_INTERPOLATION_BICUBIC)
+     * @param progressiveBilinear if true, this method will use a multi-step
+     *                            scaling technique that provides higher quality than the usual
+     *                            one-step technique (only useful in down-scaling cases, where
+     *                            targetWidth or targetHeight is
+     *                            smaller than the original dimensions)
+     * @return a scaled version of the original BufferedImage
+     */
+    public static BufferedImage getFasterScaledInstance(BufferedImage img,
+                                                        int targetWidth, int targetHeight, Object hint,
+                                                        boolean progressiveBilinear) {
+        int type = (img.getTransparency() == Transparency.OPAQUE) ?
+                BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
+        BufferedImage ret = img;
+        BufferedImage scratchImage = null;
+        Graphics2D g2 = null;
+        int w, h;
+        int prevW = ret.getWidth();
+        int prevH = ret.getHeight();
+        boolean isTranslucent = img.getTransparency() != Transparency.OPAQUE;
+
+        if (progressiveBilinear) {
+            // Use multi-step technique: start with original size, then
+            // scale down in multiple passes with drawImage()
+            // until the target size is reached
+            w = img.getWidth();
+            h = img.getHeight();
+        }
+        else {
+            // Use one-step technique: scale directly from original
+            // size to target size with a single drawImage() call
+            w = targetWidth;
+            h = targetHeight;
+        }
+
+        do {
+            if (progressiveBilinear && w > targetWidth) {
+                w /= 2;
+                if (w < targetWidth) {
+                    w = targetWidth;
+                }
+            }
+
+            if (progressiveBilinear && h > targetHeight) {
+                h /= 2;
+                if (h < targetHeight) {
+                    h = targetHeight;
+                }
+            }
+
+            if (scratchImage == null || isTranslucent) {
+                // Use a single scratch buffer for all iterations
+                // and then copy to the final, correctly-sized image
+                // before returning
+                scratchImage = new BufferedImage(w, h, type);
+                g2 = scratchImage.createGraphics();
+            }
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+            g2.drawImage(ret, 0, 0, w, h, 0, 0, prevW, prevH, null);
+            prevW = w;
+            prevH = h;
+
+            ret = scratchImage;
+        }
+        while (w != targetWidth || h != targetHeight);
+
+        if (g2 != null) {
+            g2.dispose();
+        }
+
+        // If we used a scratch buffer that is larger than our target size,
+        // create an image of the right size and copy the results into it
+        if (targetWidth != ret.getWidth() || targetHeight != ret.getHeight()) {
+            scratchImage = new BufferedImage(targetWidth, targetHeight, type);
+            g2 = scratchImage.createGraphics();
+            g2.drawImage(ret, 0, 0, null);
+            g2.dispose();
+            ret = scratchImage;
+        }
+
+        return ret;
+    }
+
     private static class GetPropertyAction
             implements java.security.PrivilegedAction {
         private String theProp;
