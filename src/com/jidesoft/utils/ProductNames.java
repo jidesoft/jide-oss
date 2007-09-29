2f8b729e0e143e7a384b7afc34fb7aebb5e28cfe
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2007-09-29 19:24:55 +0000

    add new id
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@270 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/utils/ProductNames.java
+++ src/com/jidesoft/utils/ProductNames.java
@@ -19,6 +19,7 @@ public interface ProductNames {
     public final static String PRODUCT_NAME_PIVOT = "JIDE Pivot Grid";
     public final static String PRODUCT_NAME_CODE_EDITOR = "JIDE Code Editor";
     public final static String PRODUCT_NAME_FEEDREADER = "JIDE Feed Reader";
+    public final static String PRODUCT_NAME_DASHBOARD = "JIDE Dashboard";
     public final static String PRODUCT_NAME_JDAF = "JIDE Desktop Application Framework";
 
     public final static String COMPONENT_PRODUCTS = PRODUCT_NAME_DOCK + ", " +
@@ -29,8 +30,9 @@ public interface ProductNames {
             PRODUCT_NAME_PIVOT + ", " +
             PRODUCT_NAME_SHORTCUT + ", " +
             PRODUCT_NAME_CODE_EDITOR + ", " +
+            PRODUCT_NAME_FEEDREADER + ", " +
             "and " +
-            PRODUCT_NAME_FEEDREADER;
+            PRODUCT_NAME_DASHBOARD;
 
     public final static String SOLUTION_PRODUCTS = PRODUCT_NAME_JDAF;
 
@@ -45,10 +47,11 @@ public interface ProductNames {
     public static final int PRODUCT_CODE_EDITOR = 0x80;
     public static final int PRODUCT_FEEDREADER = 0x100;
     public static final int PRODUCT_JDAF = 0x200;
+    public static final int PRODUCT_DASHBOARD = 0x400;
 
     public static final int PRODUCT_PROFESSIONAL_SUITE = PRODUCT_COMMON | PRODUCT_ACTION | PRODUCT_DOCK;
     public static final int PRODUCT_ENTERPRISE_SUITE = PRODUCT_PROFESSIONAL_SUITE | PRODUCT_COMPONENTS | PRODUCT_GRIDS | PRODUCT_DIALOGS;
-    public static final int PRODUCT_ULTIMATE_SUITE = PRODUCT_ENTERPRISE_SUITE | PRODUCT_PIVOT | PRODUCT_SHORTCUT | PRODUCT_FEEDREADER | PRODUCT_CODE_EDITOR;
+    public static final int PRODUCT_ULTIMATE_SUITE = PRODUCT_ENTERPRISE_SUITE | PRODUCT_PIVOT | PRODUCT_SHORTCUT | PRODUCT_FEEDREADER | PRODUCT_CODE_EDITOR | PRODUCT_DASHBOARD;
     public static final int PRODUCT_SOLUTION = PRODUCT_JDAF;
     public static final int PRODUCT_ALL = 0xFFFF;
 
