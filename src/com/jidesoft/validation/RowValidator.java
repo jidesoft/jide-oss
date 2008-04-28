d074edad3a79878f7d0a1a2305eba03d870bb50f
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-04-28 20:16:47 +0000

    javadoc
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@575 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/validation/RowValidator.java
+++ src/com/jidesoft/validation/RowValidator.java
@@ -10,14 +10,14 @@ public interface RowValidator extends EventListener {
     /**
      * Validates the value in the RowValidationObject.
      * <p/>
-     * The event has the row index and the JTable which is the source.
-     * The implementation of this method should validate if the
-     * row is valid. If the row is valid, simply return ValidationResult.OK.
-     * Otherwise, constructs a ValidationResult and provide the id and message.
-     * You can also return RowValidationResult which has an array of invalid column indices.
-     * Usually when you validate a row, you will be able to figure out values in one or several columns
-     * are invalid. You can pass them to RowValidationResult and return it. Listeners for the RowValidator can decide to display
-     * some error indications to indicate which cells are invalid.
+     * The event has the row index and the JTable which is the source. The implementation of this method should validate
+     * if the row is valid. If the row is valid, simply return ValidationResult.OK. Otherwise, constructs a
+     * ValidationResult and provide the id and message. You can also return RowValidationResult which has an array of
+     * invalid column indices. Usually when you validate a row, you will be able to figure out values in one or several
+     * columns are invalid. You can pass them to RowValidationResult and return it. Listeners for the RowValidator can
+     * decide to display some error indications to indicate which cells are invalid.
+     * <p/>
+     * Please note all the RowValidators added to a JideTable will be used for to validate any rows.
      *
      * @param vo the ValidationObject
      * @return ValidationResult
