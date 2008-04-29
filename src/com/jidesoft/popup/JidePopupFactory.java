bdbc69c191d49d1b359fb9308a542f84c318c274
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-04-29 03:25:59 +0000

    use JidePopupFactory.
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@576 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/popup/JidePopupFactory.java
+++ src/com/jidesoft/popup/JidePopupFactory.java
@@ -7,79 +7,73 @@
 package com.jidesoft.popup;
 
 /**
- * This class creates instances of JidePopup components.
- * It provides a consistent means for customizing popups without the need for
- * a custom UI or overriding all methods that create popups
- * 
+ * This class creates instances of JidePopup components. It provides a consistent means for customizing popups without
+ * the need for a custom UI or overriding all methods that create popups
  */
 public class JidePopupFactory {
-  private static int              popupType = -1;
-  private static JidePopupFactory popupFactory;
+    private static int _popupType = JidePopup.LIGHT_WEIGHT_POPUP;
+    private static JidePopupFactory _popupFactory;
 
-  public JidePopup createPopup() {
-    JidePopup p = new JidePopup();
+    public JidePopup createPopup() {
+        JidePopup p = new JidePopup();
 
-    if (popupType != -1) {
-      p.setPopupType(popupType);
+        if (_popupType != -1) {
+            p.setPopupType(_popupType);
+        }
+
+        return p;
     }
 
-    return p;
-  }
+    /**
+     * Sets the type to automatically create. This forces all created popups to be of the specifeid type. By default, we
+     * use LIGHT_WEIGHT_POPUP.
+     *
+     * @param poupType the popup type
+     */
+    public static void setPopupType(int poupType) {
+        if ((poupType != JidePopup.LIGHT_WEIGHT_POPUP) && (poupType != JidePopup.HEAVY_WEIGHT_POPUP)) {
+            throw new IllegalArgumentException(
+                    "invalid popup type. It must be JidePopup.HEAVY_WEIGHT_POPUP or JidePopup.LIGHT_WEIGHT_POPUP.");
+        }
 
-  /**
-   * Sets the type to automatically create.
-   * This forces all created popups to be of the specifeid type
-   *
-   * @param type the popup type
-   */
-  public static void setPopupType(int type) {
-    if ((type != JidePopup.LIGHT_WEIGHT_POPUP) && (type != JidePopup.HEAVY_WEIGHT_POPUP)) {
-      throw new IllegalArgumentException(
-          "invalid popup type. It must be JidePopup.HEAVY_WEIGHT_POPUP or JidePopup.LIGHT_WEIGHT_POPUP.");
+        _popupType = poupType;
     }
 
-    popupType = type;
-  }
+    /**
+     * Sets the <code>JidePopupFactory</code> that will be used to obtain <code>JidePopup</code>s. This will throw an
+     * <code>IllegalArgumentException</code> if <code>factory</code> is null.
+     *
+     * @param factory the shared factory
+     * @throws IllegalArgumentException if <code>factory</code> is null
+     * @see #createPopup
+     */
+    public static void setSharedInstance(JidePopupFactory factory) {
+        if (factory == null) {
+            throw new IllegalArgumentException("JidePopupFactory can not be null");
+        }
 
-  /**
-   * Sets the <code>JidePopupFactory</code> that will be used to obtain
-   * <code>JidePopup</code>s.
-   * This will throw an <code>IllegalArgumentException</code> if
-   * <code>factory</code> is null.
-   *
-   * @param factory the shared factory
-   * @exception IllegalArgumentException if <code>factory</code> is null
-   * @see #createPopup
-   */
-  public static void setSharedInstance(JidePopupFactory factory) {
-    if (factory == null) {
-      throw new IllegalArgumentException("JidePopupFactory can not be null");
+        _popupFactory = factory;
     }
 
-    popupFactory = factory;
-  }
-
-  /**
-   * Get the type of popups that will automatically be created
-   *
-   * @return the type of popups that will automatically be created or -1
-   * if the factory won't force a popup type.
-   */
-  public static int getPopupType() {
-    return popupType;
-  }
-
-  /**
-   * Returns the shared <code>JidePopupFactory</code> which can be used
-   * to obtain <code>JidePopup</code>s.
-   *
-   * @return the shared factory
-   */
-  public static JidePopupFactory getSharedInstance() {
-    if (popupFactory == null) {
-      popupFactory = new JidePopupFactory();
+    /**
+     * Get the type of popups that will automatically be created
+     *
+     * @return the type of popups that will automatically be created or -1 if the factory won't force a popup type.
+     */
+    public static int getPopupType() {
+        return _popupType;
     }
 
-    return popupFactory;
-  }
+    /**
+     * Returns the shared <code>JidePopupFactory</code> which can be used to obtain <code>JidePopup</code>s.
+     *
+     * @return the shared factory
+     */
+    public static JidePopupFactory getSharedInstance() {
+        if (_popupFactory == null) {
+            _popupFactory = new JidePopupFactory();
+        }
+
+        return _popupFactory;
+    }
 }
