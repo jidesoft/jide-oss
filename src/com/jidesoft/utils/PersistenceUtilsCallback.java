9ea99678682f55034ff0d5e4c496aeea2b93a039
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-05-22 06:52:13 +0000

    callback support in PivotTablePersistenceUtils
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@614 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/utils/PersistenceUtilsCallback.java
+++ src/com/jidesoft/utils/PersistenceUtilsCallback.java
@@ -8,22 +8,25 @@ import org.w3c.dom.Element;
  * the xml element.
  */
 public interface PersistenceUtilsCallback {
+    public static interface Save {
+        /**
+         * This method is called when writing the object to the element.
+         *
+         * @param document the XML document
+         * @param element  the element representing the object
+         * @param object   the object to be saved.
+         */
+        public void save(Document document, Element element, Object object);
+    }
 
-    /**
-     * This method is called when writing the object to the element.
-     *
-     * @param document the XML document
-     * @param element  the element representing the object
-     * @param object   the object to be saved.
-     */
-    public void save(Document document, Element element, Object object);
-
-    /**
-     * This method is called when reading the object from the element.
-     *
-     * @param document the XML document
-     * @param element  the element representing the object
-     * @param object   the object to be written.
-     */
-    public void load(Document document, Element element, Object object);
+    public static interface Load {
+        /**
+         * This method is called when reading the object from the element.
+         *
+         * @param document the XML document
+         * @param element  the element representing the object
+         * @param object   the object to be written.
+         */
+        public void load(Document document, Element element, Object object);
+    }
 }
