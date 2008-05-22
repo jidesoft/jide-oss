0e756aaea545bb68c1b71b00e85b05b85d784b04
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-05-22 06:43:40 +0000

    add callback for XxxPersistenceUtils
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@613 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

---
+++ src/com/jidesoft/utils/PersistenceUtilsCallback.java
@@ -0,0 +1,29 @@
+package com.jidesoft.utils;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * This interface is used by several PersistenceUtils classes inside JIDE to register a callback when writing or reading
+ * the xml element.
+ */
+public interface PersistenceUtilsCallback {
+
+    /**
+     * This method is called when writing the object to the element.
+     *
+     * @param document the XML document
+     * @param element  the element representing the object
+     * @param object   the object to be saved.
+     */
+    public void save(Document document, Element element, Object object);
+
+    /**
+     * This method is called when reading the object from the element.
+     *
+     * @param document the XML document
+     * @param element  the element representing the object
+     * @param object   the object to be written.
+     */
+    public void load(Document document, Element element, Object object);
+}
