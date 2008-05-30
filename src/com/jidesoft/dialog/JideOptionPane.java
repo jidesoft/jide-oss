2149aeeb62a1f80b9de7f147deb5ac150ebba34f
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-05-30 03:41:31 +0000

    add set/isDetailsVisible
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@628 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/dialog/JideOptionPane.java
+++ src/com/jidesoft/dialog/JideOptionPane.java
@@ -6,14 +6,16 @@
 
 package com.jidesoft.dialog;
 
+import com.jidesoft.plaf.basic.BasicJideOptionPaneUI;
+
 import javax.swing.*;
 
 /**
  * <code>JideOptionPane</code> is an enhanced version of JOptionPane.
  * <p/>
- * This component is still in beta, thus we didn't include the UIDefault needed by this component into LookAndFeelFactory by default.
- * If you want to use it, please refer to JideOptionPaneDemo's getDemoPanel method where we add all necessary UIDefaults
- * using UIDefaultCustomizer.
+ * This component is still in beta, thus we didn't include the UIDefault needed by this component into
+ * LookAndFeelFactory by default. If you want to use it, please refer to JideOptionPaneDemo's getDemoPanel method where
+ * we add all necessary UIDefaults using UIDefaultCustomizer.
  */
 public class JideOptionPane extends JOptionPane {
     private Object _title;
@@ -57,12 +59,15 @@ public class JideOptionPane extends JOptionPane {
         super(message, messageType, optionType, icon, options, initialValue);
     }
 
+    /**
+     * A new type for the option pane to have only the close button.
+     */
     public static final int CLOSE_OPTION = 3;
 
     /**
      * Overrides the method in JOptionPane to allow a new option - CLOSE_OPTION.
      *
-     * @param newType
+     * @param newType the type of the option pane.
      */
     @Override
     public void setOptionType(int newType) {
@@ -78,12 +83,11 @@ public class JideOptionPane extends JOptionPane {
     }
 
     /**
-     * Sets the details object. The object can be a string or a component. If it is a string,
-     * it will be put into a JTextArea. If it is a component, it will be used directly.
-     * As long as the value is not null, a "Details" button will be added to button panel
-     * allowing you to show or hide the details panel.
+     * Sets the details object. The object can be a string or a component. If it is a string, it will be put into a
+     * JTextArea. If it is a component, it will be used directly. As long as the value is not null, a "Details" button
+     * will be added to button panel allowing you to show or hide the details panel.
      *
-     * @param details
+     * @param details the details.
      */
     public void setDetails(Object details) {
         Object oldDetails = _details;
@@ -91,18 +95,56 @@ public class JideOptionPane extends JOptionPane {
         firePropertyChange(DETAILS_PROPERTY, oldDetails, _details);
     }
 
+    /**
+     * Gets the details object. The object can be a string or a component. If it is a string, it will be put into a
+     * JTextArea. If it is a component, it will be used directly. As long as the value is not null, a "Details" button
+     * will be added to button panel allowing you to show or hide the details panel.
+     *
+     * @return the details object.
+     */
     public Object getDetails() {
         return _details;
     }
 
+    /**
+     * Gets the title of the option pane.
+     *
+     * @return the title of the option pane.
+     */
     public Object getTitle() {
         return _title;
     }
 
+    /**
+     * Sets the title of the option pane.
+     *
+     * @param title the new title of the option pane.
+     */
     public void setTitle(Object title) {
         Object old = _title;
         _title = title;
         firePropertyChange(TITLE_PROPERTY, old, _title);
+    }
+
+    /**
+     * Sets the details component visible. Please note that you need to call this method before the option pane is
+     * shown. The visible flag is actually stored on a static field so if you set one option pane visible, all option
+     * panes' details component will be visible.
+     *
+     * @param visible true or false.
+     */
+    public void setDetailsVisible(boolean visible) {
+        BasicJideOptionPaneUI.setDetailsVisible(visible);
+        updateUI();
+    }
+
+    /**
+     * Checks if the details component is visible.
+     *
+     * @return true if visible. Otherwise false.
+     */
+    public boolean isDetailsVisible() {
+        return BasicJideOptionPaneUI.isDetailsVisible();
     }
 
 //    public boolean isBannerVisible() {
