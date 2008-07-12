6cf6ca516769856baf90142cda06c5a689557c51
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-07-12 15:58:10 +0000

    add TypeUtils
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@729 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

---
+++ src/com/jidesoft/utils/TypeUtils.java
@@ -0,0 +1,49 @@
+package com.jidesoft.utils;
+
+/**
+ * Utils methods for data type.
+ */
+public class TypeUtils {
+    // indexes refering to columns in the PRIMITIVE_ARRAY_TYPES table.
+    private static final int WRAPPER_TYPE_INDEX = 0;
+    private static final int PRIMITIVE_TYPE_INDEX = 1;
+    private static final int PRIMITIVE_TYPE_KEY_INDEX = 2;
+
+    private static final Object[][] PRIMITIVE_ARRAY_TYPES = {
+            {Boolean.class, boolean.class, "Z"},
+            {Character.class, char.class, "C"},
+            {Byte.class, byte.class, "B"},
+            {Short.class, short.class, "S"},
+            {Integer.class, int.class, "I"},
+            {Long.class, long.class, "J"},
+            {Float.class, float.class, "F"},
+            {Double.class, double.class, "D"}
+    };
+
+    public static boolean isPrimitive(Class<?> primitive) {
+        for (Object[] primitiveArrayType : PRIMITIVE_ARRAY_TYPES) {
+            if (primitiveArrayType[PRIMITIVE_TYPE_INDEX] == primitive) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Class<?> convertPrimitiveToWrapperType(Class<?> primitive) {
+        for (Object[] primitiveArrayType : PRIMITIVE_ARRAY_TYPES) {
+            if (primitiveArrayType[PRIMITIVE_TYPE_INDEX] == primitive) {
+                return (Class<?>) primitiveArrayType[WRAPPER_TYPE_INDEX];
+            }
+        }
+        return primitive;
+    }
+
+    public static Class<?> convertWrapperToPrimitiveType(Class<?> primitive) {
+        for (Object[] primitiveArrayType : PRIMITIVE_ARRAY_TYPES) {
+            if (primitiveArrayType[PRIMITIVE_TYPE_INDEX] == primitive) {
+                return (Class<?>) primitiveArrayType[PRIMITIVE_TYPE_INDEX];
+            }
+        }
+        return primitive;
+    }
+}
