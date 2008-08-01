5488bb25bfc5b977ff24405c60dfa883a43eadee
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-08-01 01:01:38 +0000

    fixed wildcard support doesn't use getXxxQualifier
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@759 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/utils/AbstractWildcardSupport.java
+++ src/com/jidesoft/utils/AbstractWildcardSupport.java
@@ -1,16 +1,19 @@
 package com.jidesoft.utils;
 
 /**
- * Abstract implementation of {@link WildcardSupport}. It implements the convert method but leave the other three methods
- * defining the wildcards to the subclass.
+ * Abstract implementation of {@link WildcardSupport}. It implements the convert method but leave the other three
+ * methods defining the wildcards to the subclass.
  */
 abstract public class AbstractWildcardSupport implements WildcardSupport {
 
     public String convert(String s) {
         // if it doesn't have the two special characters we support, we don't need to use regular expression.
-        int posAny = s.indexOf(getZeroOrMoreQuantifier());
-        int posOne = s.indexOf(getZeroOrOneQuantifier());
-        int posOneOrMore = s.indexOf(getOneOrMoreQuantifier());
+        char zeroOrMoreQuantifier = getZeroOrMoreQuantifier();
+        int posAny = zeroOrMoreQuantifier == 0 ? -1 : s.indexOf(zeroOrMoreQuantifier);
+        char zeroOrOneQuantifier = getZeroOrOneQuantifier();
+        int posOne = zeroOrOneQuantifier == 0 ? -1 : s.indexOf(zeroOrOneQuantifier);
+        char oneOrMoreQuantifier = getOneOrMoreQuantifier();
+        int posOneOrMore = oneOrMoreQuantifier == 0 ? -1 : s.indexOf(oneOrMoreQuantifier);
         //
         if (posAny == -1 && posOne == -1 && posOneOrMore == -1) {
             return s;
@@ -21,12 +24,14 @@ abstract public class AbstractWildcardSupport implements WildcardSupport {
         buffer.append('^');
         for (int i = 0; i < length; i++) {
             char c = s.charAt(i);
-            // replace '?' with '.'
-            if (c == '?') {
+            if (zeroOrOneQuantifier != 0 && c == zeroOrOneQuantifier) {
                 buffer.append(".");
             }
-            else if (c == '*') {
+            else if (zeroOrMoreQuantifier != 0 && c == zeroOrMoreQuantifier) {
                 buffer.append(".*");
+            }
+            else if (oneOrMoreQuantifier != 0 && c == oneOrMoreQuantifier) {
+                buffer.append("..*");
             }
             else if ("(){}[].^$\\".indexOf(c) != -1) { // escape all other special characters
                 buffer.append('\\');
