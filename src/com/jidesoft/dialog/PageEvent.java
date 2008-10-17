443893e2d605dd3ce222e964de3e7a3eed3e132d
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-10-17 04:49:51 +0000

    Add javadoc for page closing event,31870
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@841 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/dialog/PageEvent.java
+++ src/com/jidesoft/dialog/PageEvent.java
@@ -26,10 +26,16 @@ public class PageEvent extends EventObject {
     /**
      * This event is delivered when the the data becomes invalid.
      */
-    public static final int PAGE_OPENED = 0 + PAGE_EVENT_FIRST;
+    public static final int PAGE_OPENED = PAGE_EVENT_FIRST;
 
     /**
-     * This event is delivered when the page is about to close.
+     * This event is delivered when the page is about to close. This event can be listened if you want to do some
+     * validation on the page before it is closed. If it fails the validation, you can call
+     * <code>AbstractPage#setAllowClosing(false)</code> to stop the closing process. The source of the event will tell
+     * you which button originated the page closing event. For example, in the Wizard, the next button, the previous
+     * button, the cancel button or the finish button all can trigger page closing event. The event's source will be the
+     * actual button. You can call button.getName() and compare it with the predefined button names defined in {@link
+     * ButtonNames} to find out which button it is.
      */
     public static final int PAGE_CLOSING = 1 + PAGE_EVENT_FIRST;
 
@@ -62,9 +68,9 @@ public class PageEvent extends EventObject {
     }
 
     /**
-     * Sets event type.
+     * Sets the event type.
      *
-     * @param id
+     * @param id the new event type.
      */
     public void setID(int id) {
         _id = id;
