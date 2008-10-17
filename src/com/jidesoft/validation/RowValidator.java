1f76ef806ff8916193c6cfce19816a4788cfb7d2
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-10-17 17:24:26 +0000

    Added javadoc for RowValidator
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@842 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/validation/RowValidator.java
+++ src/com/jidesoft/validation/RowValidator.java
@@ -17,6 +17,23 @@ public interface RowValidator extends EventListener {
      * columns are invalid. You can pass them to RowValidationResult and return it. Listeners for the RowValidator can
      * decide to display some error indications to indicate which cells are invalid.
      * <p/>
+     * Although <code>ValidationResult</code> allows user to specify the fail behavior such as FAIL_BEHAVIOR_REVERT,
+     * FAIL_BEHAVIOR_RESET or FAIL_BEHAVIOR_PERSIST, in the case of RowValidator, none of the options make sense so the
+     * fail behavior you set will be ignored. So it is better you process the error right there in the validating
+     * method. See the code below for an example.
+     * <code><pre>
+     * public ValidationResult validating(RowValidationObject vo) {
+     *     boolean valid = ...; // validate the row
+     *     if (valid) {
+     *         return null;
+     *     }
+     *     else {
+     *         PortingUtils.notifyUser();  // notify the user
+     *         table.editCellAt(vo.getRowIndex(), 5); // force editing mode in case this is the invalid cell.
+     *         return new RowValidationResult(false, new int[]{5});
+     *     }
+     * } </pre></code>
+     * <p/>
      * Please note all the RowValidators added to a JideTable will be used for to validate any rows.
      *
      * @param vo the ValidationObject
