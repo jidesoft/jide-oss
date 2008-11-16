c0efb3b49e2a2c86f8550e193518be349b4b4031
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2008-11-16 18:43:44 +0000

    Add ResourceProvider
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@861 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

---
+++ src/com/jidesoft/utils/ResourceProvider.java
@@ -0,0 +1,12 @@
+/*
+ * @(#)ResourceProvider.java 11/15/2008
+ *
+ * Copyright 2002 - 2008 JIDE Software Inc. All rights reserved.
+ *
+ */
+
+package com.jidesoft.utils;
+
+public interface ResourceProvider {
+    String getResourceString(String key);
+}
