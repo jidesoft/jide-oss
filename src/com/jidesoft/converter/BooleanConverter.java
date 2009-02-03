cceebef522c07732692791e191dba36e7484b970
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2009-02-03 19:14:14 +0000

    Common,B,BooleanConverter,Fixed extra space at converter_nl.properties causing BooleanComboBox doesn't work when user types in the boolean string directly
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@927 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/converter/BooleanConverter.java
+++ src/com/jidesoft/converter/BooleanConverter.java
@@ -17,10 +17,10 @@ public class BooleanConverter implements ObjectConverter {
 
     public String toString(Object object, ConverterContext context) {
         if (Boolean.FALSE.equals(object)) {
-            return Resource.getResourceBundle(Locale.getDefault()).getString("Boolean.false");
+            return getFalse();
         }
         else if (Boolean.TRUE.equals(object)) {
-            return Resource.getResourceBundle(Locale.getDefault()).getString("Boolean.true");
+            return getTrue();
         }
         else {
             return "";
@@ -32,14 +32,14 @@ public class BooleanConverter implements ObjectConverter {
     }
 
     public Object fromString(String string, ConverterContext context) {
-        if (string.equalsIgnoreCase(Resource.getResourceBundle(Locale.getDefault()).getString("Boolean.true"))) {
+        if (string.equalsIgnoreCase(getTrue())) {
             return Boolean.TRUE;
         }
         else
         if (string.equalsIgnoreCase("true")) { // in case the application runs under different locale, we still condier "true" is true.
             return Boolean.TRUE;
         }
-        else if (string.equalsIgnoreCase(Resource.getResourceBundle(Locale.getDefault()).getString("Boolean.false"))) {
+        else if (string.equalsIgnoreCase(getFalse())) {
             return Boolean.FALSE;
         }
         else
@@ -54,4 +54,15 @@ public class BooleanConverter implements ObjectConverter {
     public boolean supportFromString(String string, ConverterContext context) {
         return true;
     }
+
+    private String getTrue() {
+        String s = Resource.getResourceBundle(Locale.getDefault()).getString("Boolean.true");
+        return s != null ? s.trim() : s;
+    }
+
+    private String getFalse() {
+        String s = Resource.getResourceBundle(Locale.getDefault()).getString("Boolean.false");
+        return s != null ? s.trim() : s;
+    }
+
 }
