6c5cad6f7a25ac1bab274c53842c08b8d6e48657
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2009-03-06 02:00:23 +0000

    Common,B,BasicPainter,Revert to use JideLabel.background instead,35272
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@961 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/plaf/basic/BasicPainter.java
+++ src/com/jidesoft/plaf/basic/BasicPainter.java
@@ -360,7 +360,7 @@ public class BasicPainter implements SwingConstants, ThemePainter {
     }
 
     public void paintContentBackground(JComponent c, Graphics g, Rectangle rect, int orientation, int state) {
-        g.setColor(UIDefaultsLookup.getColor("Label.background"));
+        g.setColor(UIDefaultsLookup.getColor("JideLabel.background"));
         g.fillRect(rect.x, rect.y, rect.width, rect.height);
     }
 
@@ -571,18 +571,18 @@ public class BasicPainter implements SwingConstants, ThemePainter {
             int tabShape = tabbedPane.getTabShape();
             int colorTheme = tabbedPane.getColorTheme();
             if (tabShape == JideTabbedPane.SHAPE_BOX) {
-                g.setColor(UIDefaultsLookup.getColor("Label.background"));
+                g.setColor(UIDefaultsLookup.getColor("JideLabel.background"));
             }
             else {
                 if (colorTheme == JideTabbedPane.COLOR_THEME_WIN2K) {
-                    g.setColor(UIDefaultsLookup.getColor("Label.background"));
+                    g.setColor(UIDefaultsLookup.getColor("JideLabel.background"));
 
                 }
                 else if (colorTheme == JideTabbedPane.COLOR_THEME_VSNET) {
                     g.setColor(UIDefaultsLookup.getColor("JideTabbedPane.tabAreaBackground"));
                 }
                 else {
-                    g.setColor(UIDefaultsLookup.getColor("Label.background"));
+                    g.setColor(UIDefaultsLookup.getColor("JideLabel.background"));
                 }
             }
             g.fillRect(rect.x, rect.y, rect.width, rect.height);
@@ -653,7 +653,7 @@ public class BasicPainter implements SwingConstants, ThemePainter {
         if (baseColor instanceof UIResource) {
             baseColor = UIDefaultsLookup.getColor("HeaderBox.background");
             if (baseColor == null) {
-                baseColor = UIDefaultsLookup.getColor("Label.background");
+                baseColor = UIDefaultsLookup.getColor("JideLabel.background");
             }
         }
         if (state == STATE_PRESSED || state == STATE_SELECTED || state == STATE_ROLLOVER) {
