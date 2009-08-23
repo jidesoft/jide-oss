70855445c169dd135a49318bf0d0333a0459090e
xujihe <xujihe@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2009-08-23 22:59:58 +0000

    Common,B,BasicJidePopupMenuUI,Fixed the issue that the menu item alignment of JidePopupMenu is incorrect in Metal L&F and the JidePopupMenu contains both JCheckBoxMenuItem and JMenuItem
    JonathanJidePopupMenuAlignment
    
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@1201 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/plaf/basic/BasicJidePopupMenuUI.java
+++ src/com/jidesoft/plaf/basic/BasicJidePopupMenuUI.java
@@ -11,17 +11,22 @@ import com.jidesoft.swing.SimpleScrollPane;
 import javax.swing.*;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicPopupMenuUI;
+import javax.swing.plaf.basic.DefaultMenuLayout;
 
 public class BasicJidePopupMenuUI extends BasicPopupMenuUI {
     public BasicJidePopupMenuUI() {
     }
 
+    @SuppressWarnings({"UnusedDeclaration"})
     public static ComponentUI createUI(JComponent c) {
         return new BasicJidePopupMenuUI();
     }
 
     @Override
     public Popup getPopup(JPopupMenu popupMenu, int x, int y) {
+        if (popupMenu.getLayout() instanceof DefaultMenuLayout) {
+            popupMenu.setLayout(new BoxLayout(popupMenu, ((DefaultMenuLayout) popupMenu.getLayout()).getAxis()));
+        }
         PopupFactory popupFactory = PopupFactory.getSharedInstance();
         SimpleScrollPane contents = new SimpleScrollPane(popupMenu, SimpleScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, SimpleScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         contents.getScrollUpButton().setOpaque(true);
