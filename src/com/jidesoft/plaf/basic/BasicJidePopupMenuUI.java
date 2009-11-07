514d7f6717e17d2234e8ffcb45b1d254d5dc7fa8
xujihe <xujihe@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2009-11-07 04:05:27 +0000

    Common,B,BasicJidePopupMenuUI,Fixed the issue that keyboard could not trigger the scroll of the JidePopupMenu,,yes
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@1297 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/plaf/basic/BasicJidePopupMenuUI.java
+++ src/com/jidesoft/plaf/basic/BasicJidePopupMenuUI.java
@@ -11,9 +11,12 @@ import com.jidesoft.swing.SimpleScrollPane;
 import com.jidesoft.utils.SystemInfo;
 
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 import javax.swing.plaf.ComponentUI;
 import javax.swing.plaf.basic.BasicPopupMenuUI;
 import javax.swing.plaf.basic.DefaultMenuLayout;
+import java.awt.*;
 
 public class BasicJidePopupMenuUI extends BasicPopupMenuUI {
     public BasicJidePopupMenuUI() {
@@ -38,8 +41,8 @@ public class BasicJidePopupMenuUI extends BasicPopupMenuUI {
      * @param y         the y origin
      * @return Popup
      */
-    public static Popup addScrollPaneIfNecessary(JPopupMenu popupMenu, int x, int y) {
-        SimpleScrollPane contents = new SimpleScrollPane(popupMenu, SimpleScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, SimpleScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    public static Popup addScrollPaneIfNecessary(final JPopupMenu popupMenu, int x, int y) {
+        final SimpleScrollPane contents = new SimpleScrollPane(popupMenu, SimpleScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, SimpleScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         if (popupMenu instanceof JidePopupMenu && popupMenu.getPreferredSize().height != ((JidePopupMenu) popupMenu).getPreferredScrollableViewportSize().height) {
             if (popupMenu.getLayout() instanceof DefaultMenuLayout && SystemInfo.isJdk6Above()) {
                 popupMenu.setLayout(new BoxLayout(popupMenu, ((DefaultMenuLayout) popupMenu.getLayout()).getAxis()));
@@ -48,6 +51,23 @@ public class BasicJidePopupMenuUI extends BasicPopupMenuUI {
             contents.getScrollUpButton().setOpaque(true);
             contents.getScrollDownButton().setOpaque(true);
             contents.setBorder(BorderFactory.createEmptyBorder());
+            Component[] components = popupMenu.getComponents();
+            for (Component component : components) {
+                if (component instanceof JMenuItem) {
+                    ((JMenuItem) component).addChangeListener(new ChangeListener() {
+                        public void stateChanged(ChangeEvent e) {
+                            if (e.getSource() instanceof JMenuItem) {
+                                if (((JMenuItem) e.getSource()).getModel().isArmed()) {
+                                    popupMenu.scrollRectToVisible(((JMenuItem) e.getSource()).getBounds());
+                                    Point position = contents.getViewport().getViewPosition();
+                                    contents.getScrollUpButton().setEnabled(position.y > 2);
+                                    contents.getScrollDownButton().setEnabled(position.y < contents.getViewport().getViewSize().height - contents.getViewport().getViewRect().height - 2);
+                                }
+                            }
+                        }
+                    });
+                }
+            }
             return popupFactory.getPopup(popupMenu.getInvoker(), contents, x, y);
         }
         else {
