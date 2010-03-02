4d59af51eff116ccdd419bd0b34d0f68739de8d3
xujihe <xujihe@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2010-03-02 23:05:03 +0000

    Common,B,DelgateAction,Added a NPE protection in restoreAction()
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/branches/trunk_2.8.x@1419 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/swing/DelegateAction.java
+++ src/com/jidesoft/swing/DelegateAction.java
@@ -87,6 +87,8 @@ abstract public class DelegateAction extends AbstractAction {
     abstract public boolean delegateActionPerformed(ActionEvent e);
 
     public static class PassthroughDelegateAction extends DelegateAction {
+        private static final long serialVersionUID = -1555177105658867899L;
+
         @Override
         public boolean delegateActionPerformed(ActionEvent e) {
             return false;
@@ -143,6 +145,9 @@ abstract public class DelegateAction extends AbstractAction {
     }
 
     public static void restoreAction(JComponent component, int condition, KeyStroke keyStroke) {
+        if (component == null) {
+            return;
+        }
         ActionListener action = component.getActionForKeyStroke(keyStroke);
         if (action instanceof DelegateAction) {
             component.registerKeyboardAction(((DelegateAction) action).getAction(), keyStroke, condition);
