c6d09f6a8500b138fbd0a1ad94559c9476d91e0f
xujihe <xujihe@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2010-05-19 22:26:24 +0000

    Common,B,LookAndFeelFactory,Fixed the issue that StatusBar was painted with a ResizableIcon by default in Synthetica L&F,46981,yes
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@1481 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/plaf/LookAndFeelFactory.java
+++ src/com/jidesoft/plaf/LookAndFeelFactory.java
@@ -1166,7 +1166,8 @@ public class LookAndFeelFactory implements ProductNames {
                             "CollapsiblePane.foreground", UIDefaultsLookup.getColor("TaskPane.titleForeground"),
                             "CollapsiblePane.emphasizedForeground", UIDefaultsLookup.getColor("TaskPane.specialTitleForeground"),
                             "StatusBarItem.border", new BorderUIResource(BorderFactory.createEmptyBorder(2, 2, 2, 2)),
-                            "StatusBar.childrenOpaque", true,
+                            "StatusBar.childrenOpaque", false,
+                            "StatusBar.paintResizableIcon", false,
 
                             "OutlookTabbedPane.buttonStyle", JideButton.TOOLBAR_STYLE,
                             "FloorTabbedPane.buttonStyle", JideButton.TOOLBAR_STYLE,
