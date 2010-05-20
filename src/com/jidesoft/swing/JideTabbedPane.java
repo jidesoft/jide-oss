21d36b09069d402e80751467d30c0f1764f6b484
xujihe <xujihe@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2010-05-20 21:47:45 +0000

    Common,B,JideTabbedPane,Fixed a potential AIOOBE issue if the customer floated the editing frame on editing started event,46710
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/branches/trunk_2.8.x@1487 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/swing/JideTabbedPane.java
+++ src/com/jidesoft/swing/JideTabbedPane.java
@@ -1449,7 +1449,7 @@ public class JideTabbedPane extends JTabbedPane {
 
     public void stopTabEditing() {
         int tabIndex = getEditingTabIndex();
-        if (tabIndex != -1) {
+        if (tabIndex != -1 && tabIndex < getTabCount()) {
             String oldTitle = getTitleAt(tabIndex);
             ((JideTabbedPaneUI) getUI()).stopTabEditing();
             String newTitle = getTitleAt(tabIndex);
