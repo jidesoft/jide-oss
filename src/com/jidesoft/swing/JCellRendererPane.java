7aeae70a6d3de723c7c24808c727b2336c8d0847
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2010-07-02 04:17:43 +0000

    Grids,B,CellRendererPane,Fixed cell renderer painting issue on TreeTable,48457,no
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@1523 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/swing/JCellRendererPane.java
+++ src/com/jidesoft/swing/JCellRendererPane.java
@@ -15,7 +15,10 @@ import java.io.ObjectOutputStream;
 
 /**
  * Copied from CellRendererPane and make it extending JComponent so that tooltips of renderer works.
+ *
+ * @deprecate no longer used in other JIDE classes.
  */
+@Deprecated
 public class JCellRendererPane extends JComponent implements Accessible {
     /**
      * Construct a CellRendererPane object.
@@ -27,8 +30,7 @@ public class JCellRendererPane extends JComponent implements Accessible {
     }
 
     /**
-     * Overridden to avoid propagating a invalidate up the tree when the
-     * cell renderer child is configured.
+     * Overridden to avoid propagating a invalidate up the tree when the cell renderer child is configured.
      */
     @Override
     public void invalidate() {
@@ -52,9 +54,7 @@ public class JCellRendererPane extends JComponent implements Accessible {
 
 
     /**
-     * If the specified component is already a child of this then we don't
-     * bother doing anything - stacking order doesn't matter for cell
-     * renderer components (CellRendererPane doesn't paint anyway).<
+     * If the specified component is already a child of this then we don't bother doing anything - stacking order doesn't matter for cell renderer components (CellRendererPane doesn't paint anyway).<
      */
     @Override
     protected void addImpl(Component x, Object constraints, int index) {
@@ -68,15 +68,10 @@ public class JCellRendererPane extends JComponent implements Accessible {
 
 
     /**
-     * Paint a cell renderer component c on graphics object g.  Before the component
-     * is drawn it's reparented to this (if that's necessary), it's bounds
-     * are set to w,h and the graphics object is (effectively) translated to x,y.
-     * If it's a JComponent, double buffering is temporarily turned off. After
-     * the component is painted it's bounds are reset to -w, -h, 0, 0 so that, if
-     * it's the last renderer component painted, it will not start consuming input.
-     * The Container p is the component we're actually drawing on, typically it's
-     * equal to this.getParent(). If shouldValidate is true the component c will be
-     * validated before painted.
+     * Paint a cell renderer component c on graphics object g.  Before the component is drawn it's reparented to this (if that's necessary), it's bounds are set to w,h and the graphics object is
+     * (effectively) translated to x,y. If it's a JComponent, double buffering is temporarily turned off. After the component is painted it's bounds are reset to -w, -h, 0, 0 so that, if it's the last
+     * renderer component painted, it will not start consuming input. The Container p is the component we're actually drawing on, typically it's equal to this.getParent(). If shouldValidate is true
+     * the component c will be validated before painted.
      */
     public void paintComponent(Graphics g, Component c, Container p, int x, int y, int w, int h, boolean shouldValidate) {
         if (c == null) {
@@ -149,13 +144,10 @@ public class JCellRendererPane extends JComponent implements Accessible {
     protected AccessibleContext accessibleContext = null;
 
     /**
-     * Gets the AccessibleContext associated with this CellRendererPane.
-     * For CellRendererPanes, the AccessibleContext takes the form of an
-     * AccessibleCellRendererPane.
-     * A new AccessibleCellRendererPane instance is created if necessary.
+     * Gets the AccessibleContext associated with this CellRendererPane. For CellRendererPanes, the AccessibleContext takes the form of an AccessibleCellRendererPane. A new AccessibleCellRendererPane
+     * instance is created if necessary.
      *
-     * @return an AccessibleCellRendererPane that serves as the
-     *         AccessibleContext of this CellRendererPane
+     * @return an AccessibleCellRendererPane that serves as the AccessibleContext of this CellRendererPane
      */
     @Override
     public AccessibleContext getAccessibleContext() {
@@ -166,17 +158,17 @@ public class JCellRendererPane extends JComponent implements Accessible {
     }
 
     /**
-     * This class implements accessibility support for the
-     * <code>CellRendererPane</code> class.
+     * This class implements accessibility support for the <code>CellRendererPane</code> class.
      */
     protected class AccessibleCellRendererPane extends AccessibleAWTContainer {
         // AccessibleContext methods
         //
+
         /**
          * Get the role of this object.
          *
-         * @return an instance of AccessibleRole describing the role of the
-         *         object
+         * @return an instance of AccessibleRole describing the role of the object
+         *
          * @see javax.accessibility.AccessibleRole
          */
         @Override
