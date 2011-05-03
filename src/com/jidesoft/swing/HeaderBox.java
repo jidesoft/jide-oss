b912788b3133ef2ee7d4753eea1f3a65ef99edb7
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2011-05-03 20:33:51 +0000

    Pivot,B,HeaderBox,Fixed the regression that pressing cancel on dragging a FieldBox could not cancel the dragging,,no
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@1733 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/swing/HeaderBox.java
+++ src/com/jidesoft/swing/HeaderBox.java
@@ -26,6 +26,7 @@ public class HeaderBox extends JideButton {
     public HeaderBox() {
         setOpaque(false);
         setRolloverEnabled(true);
+        setRequestFocusEnabled(true);
     }
 
     /**
