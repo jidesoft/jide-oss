9d0d5cdda2c5bc90d71dbdf5894a2adde7921dd6
support@jidesoft.com <support@jidesoft.com@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2011-08-11 03:43:52 +0000

    javadoc
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@1855 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/plaf/LookAndFeelFactory.java
+++ src/com/jidesoft/plaf/LookAndFeelFactory.java
@@ -47,48 +47,69 @@ import java.util.Vector;
  * methods. Basically, you set L&F using UIManager first just like before, then call installJideExtension. See code
  * below for an example.
  * <code><pre>
- * UIManager.setLookAndFeel(WindowsLookAndFeel.class.getName()); // you need to catch the
- * exceptions
- * on this call.
+ * UIManager.setLookAndFeel(WindowsLookAndFeel.class.getName()); // you need to catch the exceptions on this call.
  * LookAndFeelFactory.installJideExtension();
  * </pre></code>
  * LookAndFeelFactory.installJideExtension() method will check what kind of L&F you set and what operating system you
- * are on and decide which style of JIDE extension it will install. Here is the rule. <ul> <li> OS: Windows XP with XP
- * theme on, L&F: Windows L&F => OFFICE2003_STYLE <li> OS: any Windows, L&F: Windows L&F => VSNET_STYLE <li> OS: Linux,
- * L&F: any L&F based on Metal L&F => VSNET_STYLE <li> OS: Mac OS X, L&F: Aqua L&F => AQUA_STYLE <li> OS: any OS, L&F:
- * Quaqua L&F => AQUA_STYLE <li> Otherwise => VSNET_STYLE </ul> There is also another installJideExtension which takes
- * an int style parameter. You can pass in {@link #VSNET_STYLE}, {@link #ECLIPSE_STYLE}, {@link #ECLIPSE3X_STYLE},
- * {@link #OFFICE2003_STYLE}, or {@link #XERTO_STYLE}. In the other word, you will make the choice of style instead of
- * letting LookAndFeelFactory to decide one for you. Please note, there is no constant defined for AQUA_STYLE. The only
- * way to use it is when you are using Aqua L&F or Quaqua L&F and you call installJideExtension() method, the one
- * without parameter.
+ * are on and decide which style of JIDE extension it will install. Here is the rule. <ul> <li> OS: Windows Vista or
+ * Windows 7, L&F: Windows L&F => OFFICE2007_STYLE </li><li> OS: Windows XP with XP theme on, L&F: Windows L&F =>
+ * OFFICE2003_STYLE <li> OS: any Windows, L&F: Windows L&F => VSNET_STYLE <li> OS: Linux, L&F: any L&F based on Metal
+ * L&F => VSNET_STYLE <li> OS: Mac OS X, L&F: Aqua L&F => AQUA_STYLE <li> OS: any OS, L&F: Quaqua L&F => AQUA_STYLE <li>
+ * Otherwise => VSNET_STYLE </ul> There is also another installJideExtension which takes an int style parameter. You can
+ * pass in {@link #VSNET_STYLE}, {@link #ECLIPSE_STYLE}, {@link #ECLIPSE3X_STYLE}, {@link #OFFICE2003_STYLE}, {@link
+ * #OFFICE2007_STYLE}, or {@link #XERTO_STYLE}. In the other word, you will make the choice of style instead of letting
+ * LookAndFeelFactory to decide one for you. Please note, there is no constant defined for AQUA_STYLE. The only way to
+ * use it is when you are using Aqua L&F or Quaqua L&F and you call installJideExtension() method, the one without
+ * parameter.
  * <p/>
- * LookAndFeelFactory supports a number of known L&Fs. You can see those L&Fs as constants whose names are something
- * like "_LNF" such as WINDOWS_LNF.
+ * Another way is to call {@link LookAndFeelFactory#installDefaultLookAndFeelAndExtension()} which will set the default
+ * L&Fs based on the OS and install JIDE extension.
  * <p/>
- * If you are using a 3rd party L&F we are not officially supporting, we might need to customize it. Here are two
- * classes you can use. The first one is {@link UIDefaultsCustomizer}. You can add a number of customizers to
- * LookAndFeelFactory. After LookAndFeelFactory installJideExtension method is called, we will call customize() method
- * on each UIDefaultsCustomizer to add additional UIDefaults you specified. You can use UIDefaultsCustomizer to do
- * things like small tweaks to UIDefaults without the hassle of creating a new style.
+ * LookAndFeelFactory supports a number of known L&Fs. Some are L&Fs in the JDK such as Metal, Windows, Aqua (on Apple
+ * JDK only), GTK. We also support some 3rd party L&Fs such as Plastic XP or Plastic 3D, Tonic, A03, Synthetica etc.
  * <p/>
- * Most likely, we will not need to use {@link UIDefaultsInitializer} if you are use L&Fs such as WindowsLookAndFeel,
- * any L&Fs based on MetalLookAndFeel, or AquaLookAndFeel etc. The only exception is Synth L&F and any L&Fs based on it.
- * The reason is we calculate all colors we will use in JIDE components from existing well-known UIDefaults. For
- * example, we will use UIManagerLookup.getColor("activeCaption") to calculate a color that we can use in dockable
- * frame's title pane. We will use UIManagerLookup.getColor("control") to calculate a color that we can use as
- * background of JIDE component. Most L&Fs will fill those UIDefaults. However in Synth L&F, those UIDefaults may or may
- * not have a valid value. You will end up with NPE later in the code when you call installJideExtension. In this case,
- * you can add those extra UIDefaults in UIDefaultsInitializer. We will call it before installJideExtension is called so
- * that those UIDefaults are there ready for us to use. This is how added support to GTK L&F and Synthetica L&F.
+ * If you are using a 3rd party L&F we are not officially supporting, you might need to customize it. Here are two
+ * classes you can use - {@link LookAndFeelFactory.UIDefaultsInitializer} and {@link
+ * LookAndFeelFactory.UIDefaultsCustomizer}.
+ * <p/>
+ * Let's start with UIDefaultsCustomizer. No matter what unknown L&F you are trying to use, LookAndFeelFactory's
+ * installJideExtension() will try to install the UIDefaults that JIDE components required. Hopefully JIDE will run on
+ * your L&F without any exception. But most likely, it won't look very good. That's why you need {@link
+ * UIDefaultsCustomizer} to customize the UIDefaults.
+ * <p/>
+ * Most likely, you will not need to use {@link UIDefaultsInitializer}. The only exception is Synth L&F and any L&Fs
+ * based on it. The reason is we calculate all colors we will use in JIDE components from existing well-known
+ * UIDefaults. For example, we will use UIManagerLookup.getColor("activeCaption") to calculate a color that we can use
+ * in dockable frame's title pane. We will use UIManagerLookup.getColor("control") to calculate a color that we can use
+ * as background of JIDE component. Most L&Fs will fill those UIDefaults. However in Synth L&F, those UIDefaults may or
+ * may not have a valid value. You will end up with NPE later in the code when you call installJideExtension. In this
+ * case, you can add those extra UIDefaults in UIDefaultsInitializer. We will call it before installJideExtension is
+ * called so that those UIDefaults are there ready for us to use. This is how added support to GTK L&F and Synthetica
+ * L&F.
+ * <p/>
+ * After you create your own UIDefaultsCustomizer or Initializer, you can call {@link
+ * #addUIDefaultsCustomizer(com.jidesoft.plaf.LookAndFeelFactory.UIDefaultsCustomizer)} or {@link
+ * #addUIDefaultsInitializer(com.jidesoft.plaf.LookAndFeelFactory.UIDefaultsInitializer)} which will make the customizer
+ * or the initializer triggered all the times. If you only want it to be used for a certain L&F, you should use {@link
+ * #registerDefaultCustomizer(String, String)} or {@link #registerDefaultInitializer(String, String)}.
+ * <p/>
+ * By default, we also use UIDefaultsCustomizer and UIDefaultsInitializer internally to provide support for non-standard
+ * L&Fs. However we look into the classes under "com.jidesoft.plaf" package for default customizers and initializers.
+ * For example, for PlasticXPLookAndFeel, the corresponding customizer is "oom.jidesoft.plaf.plasticxp.PlasticXPCustomizer".
+ * We basically take the L&F name "PlasticXP", append it after "com.jidesoft.plaf" using lower case to get package name,
+ * take the L&F name, append with "Customizer" to get the class name. We will look at PlasticXPLookAndFeel's super class
+ * which is PlasticLookAndFeel. The customizer corresponding to PlasticLookAndFeel is
+ * "com.jidesoft.plaf.plastic.PlasticCustomizer". This searching process continues till we find all super classes of a
+ * L&F. Then we start from its top-most super class and call the customizers one by one, if it is there. The
+ * src-plaf.jar or src-plaf-jdk7.jar contain some of these customizers. You could use this naming pattern to create the
+ * customizers so that you don't need to register them explicitly.
  * <p/>
  * {@link #installJideExtension()} method will only add the additional UIDefaults to current ClassLoader. If you have
  * several class loaders in your system, you probably should tell the UIManager to use the class loader that called
  * <code>installJideExtension</code>. Otherwise, you might some unexpected errors. Here is how to specify the class
  * loaders.
  * <code><pre>
- * UIManager.put("ClassLoader", currentClass.getClassLoader()); // currentClass is the class where
- * the code is.
+ * UIManager.put("ClassLoader", currentClass.getClassLoader()); // currentClass is the class where the code is.
  * LookAndFeelFactory.installDefaultLookAndFeelAndExtension(); // or installJideExtension()
  * </pre></code>
  */
@@ -125,7 +146,7 @@ public class LookAndFeelFactory implements ProductNames {
     public static final String QUAQUA_LNF = "ch.randelshofer.quaqua.QuaquaLookAndFeel";
 
     /**
-     * Class name of Quaqua Alloy L&F.
+     * Class name of Alloy L&F.
      */
     public static final String ALLOY_LNF = "com.incors.plaf.alloy.AlloyLookAndFeel";
 
@@ -434,7 +455,8 @@ public class LookAndFeelFactory implements ProductNames {
 
     /**
      * Adds additional UIDefaults JIDE needed to UIDefault table. You must call this method every time switching look
-     * and feel. And callupdateComponentTreeUI() in corresponding DockingManager or DockableBarManager after this call.
+     * and feel. And call updateComponentTreeUI() in corresponding DockingManager or DockableBarManager after this
+     * call.
      * <pre><code>
      *  try {
      *      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -859,6 +881,12 @@ public class LookAndFeelFactory implements ProductNames {
     private static Map<String, String> _defaultInitializers;
     private static Map<String, String> _defaultCustomizers;
 
+    /**
+     * Registers a UIDefaultsInitializer with a L&F. Note that you can only register one initializer for a L&F.
+     *
+     * @param lnfClassName         full class name of the L&F
+     * @param initializerClassName full class name of the UIDefaultInitializer
+     */
     public static void registerDefaultInitializer(String lnfClassName, String initializerClassName) {
         if (_defaultInitializers == null) {
             _defaultInitializers = new HashMap<String, String>();
@@ -866,31 +894,53 @@ public class LookAndFeelFactory implements ProductNames {
         _defaultInitializers.put(lnfClassName, initializerClassName);
     }
 
-    public static void unregisterDefaultInitializer(String LnfClassName) {
+    /**
+     * Unregisters a UIDefaultsInitializer for L&F.
+     *
+     * @param lnfClassName full class name of the L&F
+     */
+    public static void unregisterDefaultInitializer(String lnfClassName) {
         if (_defaultInitializers != null) {
-            _defaultInitializers.remove(LnfClassName);
+            _defaultInitializers.remove(lnfClassName);
         }
     }
 
+    /**
+     * Clears all registered initializers.
+     */
     public static void clearDefaultInitializers() {
         if (_defaultInitializers != null) {
             _defaultInitializers.clear();
         }
     }
 
-    public static void registerDefaultCustomizer(String lnfClassName, String CustomizerClassName) {
+    /**
+     * Registers a UIDefaultsCustomizer with a L&F. Note that you can only register one customizer for a L&F.
+     *
+     * @param lnfClassName        full class name of the L&F
+     * @param customizerClassName full class name of the UIDefaultsCustomizer
+     */
+    public static void registerDefaultCustomizer(String lnfClassName, String customizerClassName) {
         if (_defaultCustomizers == null) {
             _defaultCustomizers = new HashMap<String, String>();
         }
-        _defaultCustomizers.put(lnfClassName, CustomizerClassName);
+        _defaultCustomizers.put(lnfClassName, customizerClassName);
     }
 
-    public static void unregisterDefaultCustomizer(String LnfClassName) {
+    /**
+     * Unregisters a UIDefaultCustomizer for L&F.
+     *
+     * @param lnfClassName full class name of the L&F
+     */
+    public static void unregisterDefaultCustomizer(String lnfClassName) {
         if (_defaultCustomizers != null) {
-            _defaultCustomizers.remove(LnfClassName);
+            _defaultCustomizers.remove(lnfClassName);
         }
     }
 
+    /**
+     * Clears all registered customizers.
+     */
     public static void clearDefaultCustomizers() {
         if (_defaultCustomizers != null) {
             _defaultCustomizers.clear();
