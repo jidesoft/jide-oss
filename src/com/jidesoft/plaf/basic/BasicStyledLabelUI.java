399197c85884cea64e2d3f02165591d8b6cca195
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2012-02-02 02:25:01 +0000

    Common,B,BasicStyledLabelUI,Fixed the preferred size calculation issue if StyledLabel has an explicit line break in its text,62684,no
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@2051 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/plaf/basic/BasicStyledLabelUI.java
+++ src/com/jidesoft/plaf/basic/BasicStyledLabelUI.java
@@ -303,7 +303,7 @@ public class BasicStyledLabelUI extends BasicLabelUI implements SwingConstants {
                     maxWidth = Math.max(width, maxWidth);
                     width = 0;
                     naturalRowCount++;
-                    if (naturalRowCount >= label.getMaxRows()) {
+                    if (label.getMaxRows() > 0 && naturalRowCount >= label.getMaxRows()) {
                         break;
                     }
                     continue;
