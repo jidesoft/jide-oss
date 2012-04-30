90f9c229bfdb2d84b49a63a0c08446e005c16e6b
jidesoftware <jidesoftware@8ffc590b-27e5-ea7f-97f6-895640bc1b3e>
2012-04-30 21:56:57 +0000

    Common,E,CachedArrayList,Improved the performance of CachedArrayList,,no
    git-svn-id: https://svn.java.net/svn/jide-oss~svn/trunk@2125 8ffc590b-27e5-ea7f-97f6-895640bc1b3e

--- src/com/jidesoft/utils/CachedArrayList.java
+++ src/com/jidesoft/utils/CachedArrayList.java
@@ -14,8 +14,10 @@ import java.util.*;
  * use {@link com.jidesoft.utils.CachedVector}.
  */
 public class CachedArrayList<E> extends ArrayList<E> {
+    private static final long serialVersionUID = 3835017332487313880L;
     private Map<Object, Integer> _indexCache;
     private boolean _lazyCaching = false;
+    private boolean _isDirty = false;
 
     public CachedArrayList() {
     }
@@ -33,7 +35,9 @@ public class CachedArrayList<E> extends ArrayList<E> {
 
     @Override
     public int indexOf(Object elem) {
-        initializeCache();
+        if (_indexCache == null || _isDirty) {
+            cacheAll();
+        }
         Integer o = _indexCache.get(elem);
         if (o != null) {
             return o;
@@ -59,7 +63,9 @@ public class CachedArrayList<E> extends ArrayList<E> {
      *
      * @param index    the index. All values above this index will be changed.
      * @param increase a positive number to increase or a negative number to decrease.
+     * @deprecated no longer being invoked since 3.4.0
      */
+    @Deprecated
     protected synchronized void adjustCache(int index, int increase) {
         if (_indexCache != null) {
             Map<Object, Integer> newCache = createCache();
@@ -89,8 +95,35 @@ public class CachedArrayList<E> extends ArrayList<E> {
      * @param index the index.
      */
     public void cacheIt(Object o, int index) {
-        if (_indexCache != null && (_indexCache.get(o) == null || index < _indexCache.get(o))) {
-            _indexCache.put(o, index);
+        if (_indexCache != null ) {
+            Integer old = _indexCache.put(o, index);
+            if (old != null && old < index) {
+                _indexCache.put(o, old);
+            }
+
+            markDirtyIfNecessary(index);
+            if (!_isDirty && !isLazyCaching()) {
+                for (int i = size() - 1; i > index; i--) {
+                    Integer oldI = _indexCache.put(get(i), i);
+                    if (oldI != null && oldI < index) {
+                        _indexCache.put(get(i), oldI);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Marks the entire cache dirty if necessary depends on where the index is.
+     * <p/>
+     * By default, the cache is marked dirty when the index is smaller than half of the size.
+     *
+     * @param index the index that is changing.
+     * @since 3.4.0
+     */
+    protected void markDirtyIfNecessary(int index) {
+        if (index < size() / 2) {
+            _isDirty = true;
         }
     }
 
@@ -107,9 +140,9 @@ public class CachedArrayList<E> extends ArrayList<E> {
 
     @Override
     public boolean add(E o) {
+        // In any cases, can't invoke add(int, E). Otherwise, StackOverflowError might be thrown.
         boolean added = super.add(o);
-        if (!isLazyCaching() && added) {
-            initializeCache();
+        if (!isLazyCaching() && _indexCache != null && added) {
             cacheIt(o, size() - 1);
         }
         return added;
@@ -117,14 +150,16 @@ public class CachedArrayList<E> extends ArrayList<E> {
 
     @Override
     public void add(int index, E element) {
+        // improve performance for most scenarios in HeaderTableModel
+        if (index == size()) {
+            add(element); // need make sure add(E) won't invoke this method. Otherwise, it will cause endless loop.
+            return;
+        }
         super.add(index, element);
         if (!isLazyCaching()) {
-            initializeCache();
-            adjustCache(index, 1);
             cacheIt(element, index);
         }
         else if (_indexCache != null) {
-            adjustCache(index, 1);
             cacheIt(element, index);
         }
     }
@@ -139,19 +174,16 @@ public class CachedArrayList<E> extends ArrayList<E> {
     public E remove(int index) {
         E element = super.remove(index);
         if (element != null) {
-            uncacheIt(element);
-            adjustCache(index, -1);
+            uncacheAll();
         }
         return element;
     }
 
     @Override
     public boolean remove(Object o) {
-        int oldIndex = indexOf(o);
         boolean removed = super.remove(o);
         if (removed) {
-            uncacheIt(o);
-            adjustCache(oldIndex, -1);
+            uncacheAll();
         }
         return removed;
     }
@@ -172,20 +204,26 @@ public class CachedArrayList<E> extends ArrayList<E> {
 
     @Override
     public boolean addAll(Collection<? extends E> c) {
+        // In any cases, can't invoke addAll(int, Collection<? extends E>). Otherwise, StackOverflowError might be thrown.
+        int index = size();
         boolean added = super.addAll(c);
-        if (added) {
-            cacheAll();
+        if (added && _indexCache != null) {
+            for (E e : c) {
+                cacheIt(e, index++);
+            }
         }
         return added;
     }
 
     @Override
     public boolean addAll(int index, Collection<? extends E> c) {
+        // improve performance for most scenarios in HeaderTableModel
+        if (index == size()) {
+            return this.addAll(c); // need make sure addAll(Collection<? extends E>) won't invoke this method. Otherwise, it will cause endless loop.
+        }
         boolean added = super.addAll(index, c);
-        initializeCache();
-        adjustCache(index, c.size());
-        for (E e : c) {
-            cacheIt(e, index++);
+        if (added) {
+            uncacheAll();
         }
         return added;
     }
@@ -193,11 +231,8 @@ public class CachedArrayList<E> extends ArrayList<E> {
     @Override
     public E set(int index, E element) {
         if (!isLazyCaching()) {
-            initializeCache();
-            E e = super.set(index, element);
-            uncacheIt(element);
-            cacheIt(element, index);
-            return e;
+            uncacheAll();
+            return super.set(index, element);
         }
         else {
             return super.set(index, element);
@@ -226,19 +261,20 @@ public class CachedArrayList<E> extends ArrayList<E> {
      */
     public void cacheAll() {
         _indexCache = createCache();
-        Integer i = 0;
-        for (Object elem : this) {
-            if (_indexCache.get(elem) == null) {
-                _indexCache.put(elem, i);
-            }
-            i++;
-        }
+//        Integer i = 0;
+//        for (Object elem : this) {
+//            if (_indexCache.get(elem) == null) {
+//                _indexCache.put(elem, i);
+//            }
+//            i++;
+//        }
 //        for (int i = 0; i < size(); i++) {
 //            _indexCache.put(get(i), i);
 //        }
-//        for (int i = size() - 1; i >= 0; i--) {
-//            _indexCache.put(get(i), i);
-//        }
+        for (int i = size() - 1; i >= 0; i--) {
+            _indexCache.put(get(i), i);
+        }
+        _isDirty = false;
     }
 
     public boolean isLazyCaching() {
@@ -257,9 +293,6 @@ public class CachedArrayList<E> extends ArrayList<E> {
         else {
             super.removeRange(fromIndex, toIndex);
             uncacheAll();
-            if (!isLazyCaching()) {
-                cacheAll();
-            }
         }
     }
 }
