5d248f57f9c9b096b6ec9ae06d7d93ca9d8a2a6d
jidesoft <support@jidesoft.com>
2012-05-16 15:26:56 -0700

    test git

--- test/com/jidesoft/swing/TestResizableWindow.java
+++ test/com/jidesoft/swing/TestResizableWindow.java
@@ -11,7 +11,7 @@ import javax.swing.JButton;
 public class TestResizableWindow {
 
     public static void main(String[] args) {
-
+        // test git
         EventQueue.invokeLater(new Runnable() {
 
             @Override
