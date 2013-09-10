2dae21a9feafd8933725fd4318baf9f621285f2b
jidesoft <support@jidesoft.com>
2013-09-10 14:31:08 -0700

    Common,B,JideTabbedPane,Fixed the tab background is wrong on Mac in DocumentPane,73183,no

--- src/com/jidesoft/plaf/aqua/AquaJideTabbedPaneUI.java
+++ src/com/jidesoft/plaf/aqua/AquaJideTabbedPaneUI.java
@@ -17,8 +17,10 @@ import java.awt.*;
 /**
  * JideTabbedPane UI implementation
  */
-public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
-    public static ComponentUI createUI(JComponent c) {
+public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI
+{
+    public static ComponentUI createUI(JComponent c)
+    {
         return new AquaJideTabbedPaneUI();
     }
 
@@ -27,33 +29,41 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
     protected void paintTabBackground(Graphics g, int tabPlacement,
                                       int tabIndex,
                                       int x, int y, int w, int h,
-                                      boolean isSelected) {
+                                      boolean isSelected)
+    {
         super.paintTabBackground(g, tabPlacement, tabIndex, x, y, w, h, isSelected);
 
-        if (_tabPane.getTabColorProvider() != null) {
+        if (_tabPane.getTabColorProvider() != null && _tabPane.getTabColorProvider().getBackgroundAt(tabIndex) != null)
+        {
             return;
         }
 
-        if (tabIndex >= 0 && tabIndex < _tabPane.getTabCount()) {
+        if (tabIndex >= 0 && tabIndex < _tabPane.getTabCount())
+        {
             Component component = _tabPane.getComponentAt(tabIndex);
-            if (component instanceof TabColorProvider && ((TabColorProvider) component).getTabBackground() != null) {
+            if (component instanceof TabColorProvider && ((TabColorProvider) component).getTabBackground() != null)
+            {
                 return;
             }
         }
 
-        if (!PAINT_TAB_BACKGROUND) {
+        if (!PAINT_TAB_BACKGROUND)
+        {
             return;
         }
 
-        if (!isSelected) {
+        if (!isSelected)
+        {
             return;
         }
 
         Color[] color = AquaJideUtils.isGraphite() ? AquaJideUtils.AQUA_GRAPHITE : AquaJideUtils.AQUA_BLUE;
 
-        if (tabRegion != null) {
+        if (tabRegion != null)
+        {
             Graphics2D g2d = (Graphics2D) g;
-            switch (tabPlacement) {
+            switch (tabPlacement)
+            {
                 case LEFT:
                     AquaJideUtils.fillAquaGradientVertical(g2d, tabRegion, color);
                     break;
@@ -74,26 +84,31 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
 
 
     @Override
-    protected void paintFocusIndicator(Graphics g, int tabPlacement, Rectangle[] rects, int tabIndex, Rectangle iconRect, Rectangle textRect, boolean isSelected) {
+    protected void paintFocusIndicator(Graphics g, int tabPlacement, Rectangle[] rects, int tabIndex, Rectangle iconRect, Rectangle textRect, boolean isSelected)
+    {
         // no focus rect here
     }
 
     @Override
-    protected boolean isRoundedCorner() {
+    protected boolean isRoundedCorner()
+    {
         return true;
     }
 
-    protected boolean isShading() {
+    protected boolean isShading()
+    {
         return true;
     }
 
     @Override
-    protected Color getBorderEdgeColor() {
+    protected Color getBorderEdgeColor()
+    {
         return _shadow;
     }
 
     @Override
-    protected TabCloseButton createNoFocusButton(int type) {
+    protected TabCloseButton createNoFocusButton(int type)
+    {
         return new AquaTabCloseButton(type);
     }
 
@@ -102,25 +117,28 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
     private static final Color COLOR2 = new Color(86, 86, 86);
     private static final Color COLOR3 = new Color(252, 252, 252);
 
-    public class AquaTabCloseButton extends TabCloseButton {
+    public class AquaTabCloseButton extends TabCloseButton
+    {
         /**
          * Resets the UI property to a value from the current look and feel.
-         *
          * @see JComponent#updateUI
          */
         @Override
-        public void updateUI() {
+        public void updateUI()
+        {
             super.updateUI();
             setMargin(new Insets(0, 0, 0, 0));
             setBorder(BorderFactory.createEmptyBorder());
             setFocusPainted(false);
         }
 
-        public AquaTabCloseButton() {
+        public AquaTabCloseButton()
+        {
             this(CLOSE_BUTTON);
         }
 
-        public AquaTabCloseButton(int type) {
+        public AquaTabCloseButton(int type)
+        {
             addMouseMotionListener(this);
             addMouseListener(this);
             setContentAreaFilled(false);
@@ -128,23 +146,28 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
         }
 
         @Override
-        public Dimension getPreferredSize() {
+        public Dimension getPreferredSize()
+        {
             return new Dimension(16, 16);
         }
 
         @Override
-        public Dimension getMinimumSize() {
+        public Dimension getMinimumSize()
+        {
             return new Dimension(5, 5);
         }
 
         @Override
-        public Dimension getMaximumSize() {
+        public Dimension getMaximumSize()
+        {
             return new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
         }
 
         @Override
-        protected void paintComponent(Graphics g) {
-            if (!isEnabled()) {
+        protected void paintComponent(Graphics g)
+        {
+            if (!isEnabled())
+            {
                 setMouseOver(false);
                 setMousePressed(false);
             }
@@ -154,30 +177,36 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
 
             Color color = g.getColor();
 
-            if (isMouseOver() && isMousePressed()) {
+            if (isMouseOver() && isMousePressed())
+            {
                 g.setColor(COLOR1);
                 g.fillOval(2, 2, getWidth() - 4, getHeight() - 4);
                 g.setColor(COLOR2);
             }
-            else if (isMouseOver()) {
+            else if (isMouseOver())
+            {
                 g.setColor(COLOR1);
                 g.fillOval(2, 2, getWidth() - 4, getHeight() - 4);
                 g.setColor(COLOR3);
             }
-            else {
+            else
+            {
                 g.setColor(COLOR1);
             }
             int centerX = getWidth() >> 1;
             int centerY = getHeight() >> 1;
-            switch (getType()) {
+            switch (getType())
+            {
                 case CLOSE_BUTTON:
-                    if (isEnabled()) {
+                    if (isEnabled())
+                    {
                         g.drawLine(centerX - 2, centerY - 2, centerX + 2, centerY + 2);
                         g.drawLine(centerX - 3, centerY - 2, centerX + 1, centerY + 2);
                         g.drawLine(centerX + 2, centerY - 2, centerX - 2, centerY + 2);
                         g.drawLine(centerX + 1, centerY - 2, centerX - 3, centerY + 2);
                     }
-                    else {
+                    else
+                    {
                         g.drawLine(centerX - 3, centerY - 3, centerX + 3, centerY + 3);
                         g.drawLine(centerX + 3, centerY - 3, centerX - 3, centerY + 3);
                     }
@@ -194,29 +223,35 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
                     //  |
                     //
                 {
-                    if (_tabPane.getTabPlacement() == TOP || _tabPane.getTabPlacement() == BOTTOM) {
+                    if (_tabPane.getTabPlacement() == TOP || _tabPane.getTabPlacement() == BOTTOM)
+                    {
                         int x = centerX + 2, y = centerY; // start point. mark as * above
-                        if (isEnabled()) {
+                        if (isEnabled())
+                        {
                             g.drawLine(x - 3, y - 3, x - 3, y + 3);
                             g.drawLine(x - 2, y - 2, x - 2, y + 2);
                             g.drawLine(x - 1, y - 1, x - 1, y + 1);
                             g.drawLine(x, y, x, y);
                         }
-                        else {
+                        else
+                        {
                             g.drawLine(x - 3, y - 3, x, y);
                             g.drawLine(x - 3, y - 3, x - 3, y + 3);
                             g.drawLine(x - 3, y + 3, x, y);
                         }
                     }
-                    else {
+                    else
+                    {
                         int x = centerX, y = centerY + 2; // start point. mark as * above
-                        if (isEnabled()) {
+                        if (isEnabled())
+                        {
                             g.drawLine(x - 3, y - 3, x + 3, y - 3);
                             g.drawLine(x - 2, y - 2, x + 2, y - 2);
                             g.drawLine(x - 1, y - 1, x + 1, y - 1);
                             g.drawLine(x, y, x, y);
                         }
-                        else {
+                        else
+                        {
                             g.drawLine(x - 3, y - 3, x, y);
                             g.drawLine(x - 3, y - 3, x + 3, y - 3);
                             g.drawLine(x + 3, y - 3, x, y);
@@ -224,7 +259,8 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
                     }
                 }
                 break;
-                case WEST_BUTTON: {
+                case WEST_BUTTON:
+                {
                     // a bit smaller on Aqua
                     //
                     //     |
@@ -237,29 +273,35 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
                     //
                     //
                     {
-                        if (_tabPane.getTabPlacement() == TOP || _tabPane.getTabPlacement() == BOTTOM) {
+                        if (_tabPane.getTabPlacement() == TOP || _tabPane.getTabPlacement() == BOTTOM)
+                        {
                             int x = centerX - 3, y = centerY; // start point. mark as * above
-                            if (isEnabled()) {
+                            if (isEnabled())
+                            {
                                 g.drawLine(x, y, x, y);
                                 g.drawLine(x + 1, y - 1, x + 1, y + 1);
                                 g.drawLine(x + 2, y - 2, x + 2, y + 2);
                                 g.drawLine(x + 3, y - 3, x + 3, y + 3);
                             }
-                            else {
+                            else
+                            {
                                 g.drawLine(x, y, x + 3, y - 3);
                                 g.drawLine(x, y, x + 3, y + 3);
                                 g.drawLine(x + 3, y - 3, x + 3, y + 3);
                             }
                         }
-                        else {
+                        else
+                        {
                             int x = centerX, y = centerY - 2; // start point. mark as * above
-                            if (isEnabled()) {
+                            if (isEnabled())
+                            {
                                 g.drawLine(x, y, x, y);
                                 g.drawLine(x - 1, y + 1, x + 1, y + 1);
                                 g.drawLine(x - 2, y + 2, x + 2, y + 2);
                                 g.drawLine(x - 3, y + 3, x + 3, y + 3);
                             }
-                            else {
+                            else
+                            {
                                 g.drawLine(x, y, x - 3, y + 3);
                                 g.drawLine(x, y, x + 3, y + 3);
                                 g.drawLine(x - 3, y + 3, x + 3, y + 3);
@@ -268,7 +310,8 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
                     }
                     break;
                 }
-                case LIST_BUTTON: {
+                case LIST_BUTTON:
+                {
                     int x = centerX, y = centerY + 2; // start point. mark as * above
                     g.drawLine(x - 3, y - 3, x + 3, y - 3);
                     g.drawLine(x - 2, y - 2, x + 2, y - 2);
@@ -284,13 +327,15 @@ public class AquaJideTabbedPaneUI extends VsnetJideTabbedPaneUI {
         }
 
         @Override
-        public boolean isOpaque() {
+        public boolean isOpaque()
+        {
             return false;
         }
     }
 
     @Override
-    protected void prepareEditor(TabEditor e, int tabIndex) {
+    protected void prepareEditor(TabEditor e, int tabIndex)
+    {
         e.setOpaque(true);
         super.prepareEditor(e, tabIndex);
     }
